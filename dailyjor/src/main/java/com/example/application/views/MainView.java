e8810d7b5ae7a8766ce1bd1661e39a7a9d0192cd
GeorgeDenis <george.duluta@yahoo.com>
2023-05-28 14:03:30 +0300

    mainUpdate

--- dailyjor/src/main/java/com/example/application/views/MainView.java
+++ dailyjor/src/main/java/com/example/application/views/MainView.java
@@ -1,5 +1,6 @@
 package com.example.application.views;
 
+import com.example.application.data.entity.User;
 import com.example.application.data.service.UserRepository;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -8,18 +9,23 @@ import com.vaadin.flow.router.RouteAlias;
 
 @Route(value = "main", layout = MenuLayout.class)
 @RouteAlias(value = "", layout = MenuLayout.class)
-
-
 public class MainView extends VerticalLayout {
 
     private final UserRepository userRepository;
+    private Label label;
+
 
     public MainView(UserRepository userRepository) {
         this.userRepository = userRepository;
+        User user = userRepository.findByIsLoggedTrue();
 
-        Label label = new Label("Welcome,   !");
+        if(user != null) {
+            label = new Label("Welcome " + user.getUsername() + "!\nDailyJournal is a Java Spring application with a Vaadin frontend that allows users to keep track of various personal activities such as reading books, watching movies, and fitness entries.");
+        }
+        else {
+            label = new Label("Welcome!\nDailyJournal is a Java Spring application with a Vaadin frontend that allows users to keep track of various personal activities such as reading books, watching movies, and fitness entries.");
+        }
+
         add(label);
     }
-
 }
-
